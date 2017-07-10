079d3af93cde492589e5c8aaa770713056b59361
Pierrot <semecurbep@gmail.com>
2017-07-10 16:25:21 +0200

    Ajout chargement fichier

--- routes.py
+++ routes.py
@@ -1,22 +1,32 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, redirect, url_for,\
+     flash
 import numpy as np
 
 import matplotlib
 import json
 import random
+import os
 
 #matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 #plt.ioff()
-
 import mpld3
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = 'data'
+ALLOWED_EXTENSIONS = set(['txt', 'fms'])
+
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in\
+           ALLOWED_EXTENSIONS
 
 def graph_test():
-    x = range(100)
+    x = range(16000)
     y = [a * 2 + random.randint(-20, 20) for a in x]
     fig, ax = plt.subplots()
     ax.plot(x, y)
@@ -26,13 +36,27 @@ def graph_test():
 
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('index.html', filename = filename)
+    if request.method == 'GET':
+        return render_template('index.html')
 
 
-@app.route('/query')
-def query():
+@app.route('/query/<filename>')
+def query(filename):
     fig_html = graph_test()
     return fig_html
 
